cc7c83c7904dbc8d83bb5bb90817ddd9623051e7
Omkar <omkarjoshiop@gmail.com>
2025-05-10 10:33:39 +0530

    datatyp, function bsc

---
+++ Basics.js
@@ -0,0 +1,82 @@
+// console.log('hey');
+
+// let a=null;
+
+
+// console.log(a);
+// console.log(typeof(a));
+// console.log(`The value is ${a}`);
+
+
+// let b=48;
+
+
+// console.log(b);
+// console.log(typeof(b));
+// console.log(`The value is ${b}`);
+
+// let c="hello";
+
+
+// console.log(c);
+// console.log(typeof(c));
+// console.log(`The value is ${c}`);
+
+
+// //bigInt
+
+// let d=2012123131231231231323n;
+// console.log(d);
+// console.log(typeof(d));
+// console.log(`The value is ${d}`);
+
+
+// let obj={
+
+//     name:"om",
+//     age:55,
+//     city:"delhi"
+// }
+// console.log(obj);
+// console.log(typeof(obj));
+// console.log(`The value is ${obj}`);
+
+
+
+// Does not support without value initailiazation for const. Supports only var and let
+// const z;
+
+
+// functional and blcok scope
+
+// function lett()
+// {
+//     let a=20;
+//     console.log(a)
+// }
+
+// lett();
+
+// function lett()
+// {
+//     {
+//     let a=20;
+//     }
+//     console.log(a)
+    
+// }
+
+// lett();
+//undefined a error   as let and const in a block scope!! 
+
+// function lett()
+// {
+//     {
+//     var a=20;
+//     }
+//     console.log(a)
+    
+// }
+
+// lett();
+// This will be accessible as VAR as functional scope
